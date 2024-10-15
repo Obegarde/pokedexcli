3fecbfed5f2bb5dcbf79436eab53712714abdb89
Obegarde <5763603+Obegarde@users.noreply.github.com>
2024-10-15 19:47:29 +0200

    Add map and mapb

--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import(
 	"os"
 	"bufio"
 	"strings"
+	"github.com/obegarde/pokedexcli/internal/pokeapi"
 )
 
 type cliCommand struct{
 	name string
 	description string
-	callback func() error
+	callback func(*pokeapi.Client) error
 }
 var cliCommands map[string]cliCommand
+
+
 
 func initializeCommands() error{
 cliCommands = map[string]cliCommand{
@@ -25,8 +28,19 @@ cliCommands = map[string]cliCommand{
 		description: "Exit the Pokedex\n",
 		callback: commandExit,
 	},
+	"map":{
+		name:"map",
+		description:"Displays the name of 20 locations in Pokemon or the next 20 locations\n",
+		callback:commandMap,
+		},
+	"mapb":{
+		name:"mapb",
+		description:"Displays the previous 20 locations\n",
+		callback:commandMapb,
+		},
 
 }
+	
 	return nil
 }
 
@@ -41,15 +55,56 @@ func getInput() (string, error){
 	
 }
 
-func commandHelp() error{
+func commandHelp(_ *pokeapi.Client) error{
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Printf("Usage: \n\n help: Displays a help message\n exit: Exit the Pokedex")
 	return nil
 }
 
-func commandExit() error{	
+func commandExit(_ *pokeapi.Client) error{	
 	return fmt.Errorf("exit") 
 }
+func commandMap(c *pokeapi.Client) error{
+	
+	if (c.LastResponse.Next == nil ){
+		err := pokeapi.GetBaseLocationAreas(c)
+
+		if err != nil{
+		return err 
+		}
+	}else{
+		err := pokeapi.GetNextLocationAreas(c)
+		if err != nil{
+		return err
+		}
+	}
+	
+	printLocations(c)	
+
+	return nil
+}
+
+
+func commandMapb(c *pokeapi.Client) error{
+	if c.LastResponse.Previous == nil{
+		return fmt.Errorf("No Previous Areas found. Please use Map") 
+		}else{
+		err := pokeapi.GetPreviousLocationAreas(c)
+		if err != nil{
+		return err
+		}
+		printLocations(c)
+	}
+	return nil
+}
+
+func printLocations(c *pokeapi.Client)error{	
+	for _, Area := range c.LastResponse.LocationArea{
+	fmt.Println(Area.Name)	
+	}
+	return nil
+}
+
 
 func main(){
 	fmt.Println("Welcome To the Pokedex!")
@@ -58,6 +113,13 @@ func main(){
 	fmt.Println("Error: initializing, exiting")
 	return
 	}
+	c, err := pokeapi.NewClient()
+	if err != nil{
+	fmt.Printf("New Client Error: %s", err)
+	return
+	}
+
+
 	for{
 	command, err := getInput()
 	if err != nil{
@@ -69,7 +131,7 @@ func main(){
 		fmt.Println("Unknown command")
 		continue
 		}
-	err = cmd.callback()
+	err = cmd.callback(c)
 	if err !=nil{
 		if err.Error() == "exit"{
 			break
